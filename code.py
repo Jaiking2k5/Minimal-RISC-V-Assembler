dc294a4b5d22a76c428b35a4ad639bb6556953f6
Jaiking2k5 <143246593+Jaiking2k5@users.noreply.github.com>
2025-08-06 17:52:27 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,4 @@
+# Basic instruction patterns
 opcodes = {
     'add':  '0110011',
     'sub':  '0110011',
@@ -7,11 +8,8 @@ opcodes = {
 }
 
 funct3 = {
-    'add':  '000',
-    'sub':  '000',
-    'addi': '000',
-    'sw':   '010',
-    'beq':  '000'
+    'add':  '000', 'sub':  '000', 'addi': '000',
+    'sw':   '010', 'beq':  '000'
 }
 
 funct7 = {
@@ -19,94 +17,71 @@ funct7 = {
     'sub':  '0100000'
 }
 
+# Turn register like "x5" into 5-bit binary like "00101"
 def reg_to_bin(reg):
-    reg_num = int(reg[1:]) 
-    return format(reg_num, '05b')
+    num = int(reg[1:])  # remove 'x'
+    return format(num, '05b')
 
-# Encode R-type instruction
+# R-type: add, sub
 def encode_r(op, rd, rs1, rs2):
     return funct7[op] + reg_to_bin(rs2) + reg_to_bin(rs1) + funct3[op] + reg_to_bin(rd) + opcodes[op]
 
-# Encode I-type instruction
+# I-type: addi
 def encode_i(op, rd, rs1, imm):
     imm_bin = format(int(imm) & 0xFFF, '012b')
     return imm_bin + reg_to_bin(rs1) + funct3[op] + reg_to_bin(rd) + opcodes[op]
 
-# Encode S-type instruction
+# S-type: sw
 def encode_s(op, rs2, rs1, imm):
-    imm_val = int(imm) & 0xFFF
-    imm_bin = format(imm_val, '012b')
-    imm_high = imm_bin[:7]
-    imm_low = imm_bin[7:]
-    return imm_high + reg_to_bin(rs2) + reg_to_bin(rs1) + funct3[op] + imm_low + opcodes[op]
+    imm_bin = format(int(imm) & 0xFFF, '012b')
+    return imm_bin[:7] + reg_to_bin(rs2) + reg_to_bin(rs1) + funct3[op] + imm_bin[7:] + opcodes[op]
 
-# Encode B-type instruction
+# B-type: beq
 def encode_b(op, rs1, rs2, imm):
-    offset = int(imm)
-    offset_bin = format(offset & 0x1FFF, '013b')
-    return offset_bin[0] + offset_bin[2:8] + reg_to_bin(rs2) + reg_to_bin(rs1) + funct3[op] + offset_bin[8:12] + offset_bin[1] + opcodes[op]
+    imm_bin = format(int(imm) & 0x1FFF, '013b')
+    # imm bits rearranged as per RISC-V spec
+    return imm_bin[0] + imm_bin[2:8] + reg_to_bin(rs2) + reg_to_bin(rs1) + funct3[op] + imm_bin[8:12] + imm_bin[1] + opcodes[op]
 
-# Convert 32-bit binary to hex
-def bin_to_hex(bin_str):
-    return format(int(bin_str, 2), '08x')
+# Binary to Hex
+def bin_to_hex(binary):
+    return format(int(binary, 2), '08x')
 
-# Convert pseudo-instructions to real ones
+# Handle pseudo instructions like li, mv, nop
 def expand_pseudo(op, args):
-    if op == 'li':
-        return [('addi', args[0], 'x0', args[1])]
-    elif op == 'mv':
-        return [('addi', args[0], args[1], '0')]
-    elif op == 'nop':
-        return [('addi', 'x0', 'x0', '0')]
-    else:
-        return [(op, *args)]
+    if op == 'li':   return [('addi', args[0], 'x0', args[1])]
+    if op == 'mv':   return [('addi', args[0], args[1], '0')]
+    if op == 'nop':  return [('addi', 'x0', 'x0', '0')]
+    return [(op, *args)]  # normal instruction
 
-def assemble(assembly):
-    result = []
+# The main function
+def assemble(asm_lines):
+    hex_output = []
 
-    for line in assembly:
+    for line in asm_lines:
         line = line.split('#')[0].strip()  # remove comments
-        if not line:
-            continue
+        if not line: continue
 
         parts = line.replace(',', '').split()
         op = parts[0]
         args = parts[1:]
 
-        instructions = expand_pseudo(op, args)
+        expanded = expand_pseudo(op, args)
 
-        for inst in instructions:
-            op2 = inst[0]
-            args2 = inst[1:]
+        for inst in expanded:
+            op2, *args2 = inst
 
             if op2 in ['add', 'sub']:
-                bin_code = encode_r(op2, args2[0], args2[1], args2[2])
+                binary = encode_r(op2, *args2)
             elif op2 == 'addi':
-                bin_code = encode_i(op2, args2[0], args2[1], args2[2])
+                binary = encode_i(op2, *args2)
             elif op2 == 'sw':
-                bin_code = encode_s(op2, args2[0], args2[2], args2[1])
+                binary = encode_s(op2, args2[0], args2[2], args2[1])  # sw x4, 0(x0)
             elif op2 == 'beq':
-                bin_code = encode_b(op2, args2[0], args2[1], args2[2])
+                binary = encode_b(op2, *args2)
             else:
-                bin_code = '0' * 32 
+                binary = '0' * 32  # fallback
 
-            hex_code = bin_to_hex(bin_code)
-            result.append(hex_code)
+            hex_output.append(bin_to_hex(binary))
 
-    return result
-    
-    program = [
-        "li x1, 5",          # -> addi x1, x0, 5
-        "li x2, 10",         # -> addi x2, x0, 10
-        "add x3, x1, x2",    # -> x3 = x1 + x2
-        "mv x4, x3",         # -> addi x4, x3, 0
-        "sw x4, 0(x0)",      # -> memory[0] = x4
-        "beq x1, x2, 4",     # -> if x1 == x2 jump
-        "nop"                # -> addi x0, x0, 0
-    ]
+    return hex_output
 
-    output = assemble(program)
-
-    print("Generated HEX:")
-    for line in output:
-        print(line)
